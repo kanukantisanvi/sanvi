e73578242edb8f6e41fb5db534b67cb5b339bfaa
k.sanvi <kanukantisanvi@gmail.com>
2025-01-14 13:46:55 +0530

    Create loops.cpp

---
+++ loops.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+//	int i=5;
+//	do{
+//		cout<<"Do While"<<i<<endl;
+//		i++;
+//	}
+//	while(i<5);
+//	
+//	int j = 10;
+//	while(j<5)
+//	{
+//		cout<<"While"<<j<<endl;
+//		j++;
+//	}
+//	int i = 0;
+//	
+//	while(i<5)
+//	{
+//		cout<<i<<endl;
+//		i++;
+//	}
+//	
+	
+//	for(int i=0;i<5;i++)
+//   {
+//		cout<<i<<endl;
+//   }
+//   
+     for(int i=1;i<=10;i++)
+     {
+     	cout<<"2 x "<<i<<"="<<(2*i)<<endl;
+	 }
+
+    return 0;
+}
