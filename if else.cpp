bdf23cd8c01386cc4755af8d9f5e52521e08a9b0
k.sanvi <kanukantisanvi@gmail.com>
2025-01-14 12:50:17 +0530

    Create if else.cpp

---
+++ if else.cpp
@@ -0,0 +1,57 @@
+sing namespace std;
+
+int main()
+{
+//	bool rain;#include<iostream>
+using namespace std;
+
+int
+//	cout<<"Is it raining?"<<endl;
+//	cin>>rain;
+//	
+//	if(rain==true)
+//	{
+//		cout<<"you cannot go to cinema"<<endl;
+//	}
+//	else
+//	{
+//		cout<<"you can go to cenima"<<endl;
+//	}
+//	
+//	int a;
+//	int b;
+//	
+//	cout<<"Enter A and B values"<<endl;
+//	cin>>a>>b;
+//	
+//	//a=10 b=10
+//	if(a<b){
+//		cout<<"B is greater"<<endl;
+//	}
+//	else if(a==b)
+//	{
+//		cout<<"Both are equal"<<endl;
+//	}
+//	else{
+//		cout<<"A is greater"<<endl;
+//	}
+
+    int a = 11;
+    int b = 20;
+    
+    if(a<b)
+    {
+    	cout<<"A is Smaller";
+    	if(a%2==0)
+    	{
+    		cout<<"A is Even";
+		}
+		else
+		{
+			cout<<"A is odd";
+		}
+	}
+
+
+	return 0;
+}
