134e5ed9fda7b389f7a2b0c7ee74637add31ad2d
k.sanvi <kanukantisanvi@gmail.com>
2025-01-14 12:11:06 +0530

    Create conditional.cpp

---
+++ conditional.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	bool rain = false;
+	
+	if(rain==true)
+	{
+		cout<<"you can go to cinema"<<endl;
+	}
+	
+	int a = 10;
+	int b = 20;
+	
+	if(a<b){
+		cout<<"B is greater"<<endl;
+	}
+	return 0;
+}
