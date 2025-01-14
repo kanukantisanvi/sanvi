115b648e01476f3e3b3e532bd43b713a063f3a7e
k.sanvi <kanukantisanvi@gmail.com>
2025-01-14 20:30:35 +0530

    Create arrays2d.cpp

---
+++ arrays2d.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{	
+   int arr[3][4] = {
+   {4,3,4,8},
+   {5,6,8,7},
+   {6,8,3,7},
+   };
+   
+    cout<<arr[2][2]<<endl;
+    
+    return 0;
+}
+   
+   
+   
